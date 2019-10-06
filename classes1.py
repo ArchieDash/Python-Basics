f84127195cf7fc375f4678f375845e10d25839f7
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-06 17:20:11 +0300

    Classes exercise
    Comments added

--- classes1.py
+++ classes1.py
@@ -4,12 +4,12 @@ import time
 class Dog():
     species = "Canis familiaris"
     
-    def __init__ (self, name, age, coat_color):
+    def __init__ (self, name, age, coat_color): #instance initialization method
         self.name = name
         self.age = age
         self.coat_color = coat_color
         
-    def __repr__ (self):
+    def __repr__ (self): #instance representation with interpolation of the params
         return f"{self.name} / age - {self.age} / {self.coat_color} coat."
 
     def speak(self, sound):
@@ -19,10 +19,10 @@ class Dog():
         return f"{self.name}'s coat is {self.coat_color}."
 
 
-class GoldenRetriever(Dog):
+class GoldenRetriever(Dog): #inheritance of the class interface
     
     def speak(self, sound="Bark"):
-        return super().speak(sound)
+        return super().speak(sound) #reference to the parent method
 
 
 class Rectangle():
@@ -42,10 +42,10 @@ class Square(Rectangle):
         self.width = side_length
 
 
-Mark = GoldenRetriever("Mark", 3, "blond")
+Mark = GoldenRetriever("Mark", 3, "blond") #GoldenRetriever instance initialization
 print(Mark.speak())
-a = Rectangle (3, 4)
+a = Rectangle (3, 4) #Rectangle instance initialization
 print(f'Area of a rectangle 3x4 is {a.area()}')
-b = Square(4)
-print(f'Area of a square 4x4 is {b.area()}')
+b = Square(4) #Square instace initialization
+print(f'Area of a square 4x4 is {b.area()}') #reference to the parent method
 time.sleep(5)
