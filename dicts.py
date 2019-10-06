8e6b53c02e1b84634d9b65c07dac202616458998
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-06 17:31:03 +0300

    Dictionaries exercise

--- dicts.py
+++ dicts.py
@@ -3,9 +3,9 @@ from collections import defaultdict
 
 
 birthdays = defaultdict(lambda: "unknown", {"Luke Skywalker":"5/24/19",
-                    "Obi-Wan Kenobi":"3/11/57", "Darth Vader":"4/1/41"})
+                    "Obi-Wan Kenobi":"3/11/57", "Darth Vader":"4/1/41"}) #set the default value for any missed key to be 'unknown'
 print(f"Yoda's birthday - {birthdays['Yoda']}\nDarth Vader's birthday - {birthdays['Darth Vader']}\n")
-del birthdays["Darth Vader"] #delete position from dict
+del birthdays["Darth Vader"] #delete position from the dict
 for key, value in birthdays.items():
     if value != "unknown":
         print(f"{key} {value}") #print non-default key-value pairs
