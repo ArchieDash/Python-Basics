0d3e5885164ec08b7109c893dbe56c545bce4492
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-06 17:23:24 +0300

    Classes exercise
    Comments edited

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@ class Dog():
         self.age = age
         self.coat_color = coat_color
         
-    def __repr__ (self):
+    def __repr__ (self): #representation of an instance with interpolated params
         return f"{self.name} / age - {self.age} / {self.coat_color} coat."
     
     def coat(self):
@@ -19,7 +19,7 @@ class Car():
         self.color = color
         self.mileage = mileage
         
-    def __repr__ (self):
+    def __repr__ (self): #representation of an instance with interpolated params
         return f"{self.color} car / mileage: {self.mileage}"
     
     def drive (self, miles):
@@ -30,8 +30,8 @@ philo = Dog('Philo', 5, 'brown') #instance of the Dog class
 print(philo.coat()) #call the instance method
 mustang = Car('blue', 20_000) #instance of the Car class
 ferrari = Car('red', 30_000) #instance of the Car class
-print(f'The {mustang.color} car has {mustang.mileage} miles') #interpolation of the instances' params
-print(f'The {ferrari.color} car has {ferrari.mileage} miles') #interpolation of the instances' params
+print(f'The {mustang.color} car has {mustang.mileage} miles') #interpolation of the instance params
+print(f'The {ferrari.color} car has {ferrari.mileage} miles') #interpolation of the instance params
 lamborghini = Car('yellow', 0) #instance of the Car class
 print(lamborghini) #check instance state with __repr__ method
 lamborghini.drive(100) #call the class method to change instance's params
