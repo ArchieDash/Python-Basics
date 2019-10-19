516ca98e3477cf6c54410df7b4e8a944e05f5964
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-19 18:37:43 +0300

    Functions exercise

---
+++ funcs.py
@@ -0,0 +1,18 @@
+import time
+
+
+def cube(number):
+    return pow(number, 3)
+
+
+def greet(name):
+    return f'Hello {name}'
+
+
+print('Task_1')
+print(cube(3))
+time.sleep(3)
+
+print('\nTask_2')
+print(greet('Alex'))
+time.sleep(3)
