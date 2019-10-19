6315e212051d6abb76003a4f113785c0f5f6c727
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-19 18:46:32 +0300

    Classes exercise

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@ class Dog():
         self.age = age
         self.coat_color = coat_color
         
-    def __repr__ (self): #representation of an instance with interpolated params
+    def __repr__ (self):  # representation of an instance with interpolated params
         return f"{self.name} / age - {self.age} / {self.coat_color} coat."
     
     def coat(self):
@@ -19,21 +19,21 @@ class Car():
         self.color = color
         self.mileage = mileage
         
-    def __repr__ (self): #representation of an instance with interpolated params
+    def __repr__ (self):  # representation of an instance with interpolated params
         return f"{self.color} car / mileage: {self.mileage}"
     
     def drive (self, miles):
         self.mileage += miles
 
 
-philo = Dog('Philo', 5, 'brown') #instance of the Dog class
-print(philo.coat()) #call the instance method
-mustang = Car('blue', 20_000) #instance of the Car class
-ferrari = Car('red', 30_000) #instance of the Car class
-print(f'The {mustang.color} car has {mustang.mileage} miles') #interpolation of the instance params
-print(f'The {ferrari.color} car has {ferrari.mileage} miles') #interpolation of the instance params
-lamborghini = Car('yellow', 0) #instance of the Car class
-print(lamborghini) #check instance state with __repr__ method
-lamborghini.drive(100) #call the class method to change instance's params
-print(lamborghini) #check instance state with __repr__ method
+philo = Dog('Philo', 5, 'brown')  # instance of the Dog class
+print(philo.coat())  # call the instance method
+mustang = Car('blue', 20_000)  # instance of the Car class
+ferrari = Car('red', 30_000)  # instance of the Car class
+print(f'The {mustang.color} car has {mustang.mileage} miles')  # interpolation of the instance params
+print(f'The {ferrari.color} car has {ferrari.mileage} miles')  # interpolation of the instance params
+lamborghini = Car('yellow', 0)  # instance of the Car class
+print(lamborghini)  # check instance state with __repr__ method
+lamborghini.drive(100)  # call the class method to change instance's params
+print(lamborghini)  # check instance state with __repr__ method
 time.sleep(8)
