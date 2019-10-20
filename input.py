0025394257cf2fdd87b48c336344bffcd79320e3
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-20 21:33:12 +0300

    User input exercise

---
+++ input.py
@@ -0,0 +1,15 @@
+import time
+
+
+print('Task_1')
+variable = input('User input: ')
+print(f'Stdout: {variable}')
+
+print('\nTask_2')
+variable = input('User input: ')
+print(f'Stdout in lowercase: {variable.lower()}')
+
+print('\nTask_3')
+variable = input('User input: ')
+print(f'Number of characters in your input: {len(variable)}')
+time.sleep(3)
