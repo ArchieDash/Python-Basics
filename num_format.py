f73b8f029e643306208ed0e7f87d0d863ac13f69
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-21 19:41:52 +0300

    Formatting numbers exercise

---
+++ num_format.py
@@ -0,0 +1,13 @@
+import time
+
+
+print('Task_1')
+num1 = 25000000
+num2 = 25_000_000
+print(f'{num1:,}\n{num2:,}')
+time.sleep(3)
+
+print('\nTask_2')
+num = 175e3
+print(f'{num:.1f}')
+time.sleep(3)
