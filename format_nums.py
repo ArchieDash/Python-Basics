81e91792a926be2c3d67436a0c41b42ba47a8bb4
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-22 20:08:53 +0300

    Formatting numbers exercise

---
+++ format_nums.py
@@ -0,0 +1,11 @@
+import time
+
+
+print(f'Task_1\n{round(3**.125, 3)}')  # round formatting with round func
+time.sleep(3)
+
+print(f'\nTask_2\n{150000:,.2f}')  # round formatting with interpolation method
+time.sleep(3)
+
+print(f'\nTask_3\n{2/10:.0%}')  # round percentage formatting
+time.sleep(3)
