84806c7d5603e62f8d62bcbe42523603ac58d17d
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-10-30 00:29:30 +0300

    Matplotlib challenge (#TODO)

--- pirates.py
+++ pirates.py
@@ -7,13 +7,15 @@ path = os.path.join('files', 'pirates.csv')
 years = list()
 temps = list()
 pirates = list()
+#  TODO: find a way to extract data without initializing empty lists variables
 with open(path, 'r') as fin:
     data = csv.reader(fin)
-    next(data)
-    for year, temp, count in data:
+    next(data)  # skip header
+    for year, temp, count in data:  # extract data from csv
         years.append(year)
         temps.append(temp)
         pirates.append(count)
+
 xs = pirates
 plt.xlabel('Count pirates')
 ys = temps
@@ -21,4 +23,6 @@ plt.ylabel('Average temperature')
 plt.plot(xs, ys, 'r-o')
 plt.title('Quantity relationship of pirates count and year average temperature')
 plt.savefig(os.path.join('files', 'pirates.png'))
+# TODO: add labels for each year
+# TODO: insert the graph to PDF template
 plt.show()
