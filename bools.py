94cf3ce4952bda9c624c40be226dfbb8956ab13b
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-12-08 15:57:32 +0300

    Docstring added

--- bools.py
+++ bools.py
@@ -1,11 +1,8 @@
-import time
-
-
-string = input('Type some string: ')
+"""Check the lenght of the input string with boolean comparison"""
+string = input("Type some string: ")
 if len(string) > 5:
-    print('Length of the string is greater than 5 chars.')
+    print("Length of the string is greater than 5 chars.")
 elif len(string) < 5:
-    print('Length of the string is less than 5 chars.')
+    print("Length of the string is less than 5 chars.")
 else:
-    print('Length of the string is equal to 5 chars.')
-time.sleep(3)
+    print("Length of the string is equal to 5 chars.")
